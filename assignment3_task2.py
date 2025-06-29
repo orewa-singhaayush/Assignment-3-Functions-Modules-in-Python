f535d96eeb963f30a84b0ed2c50a654f189168bd
orewa-singhaayush <21asksingh17034@gmail.com>
2025-06-29 11:37:10 +0530

    Add files via upload

---
+++ assignment3_task2.py
@@ -0,0 +1,6 @@
+num=int(input("Enter a number:"))
+import math
+
+print("The square root of",num,"is = ",math.sqrt(num))
+print("The sine of",num,"is = ",math.sin(num))
+print("The logarithmic of ",num,"is = ",math.log(num))
